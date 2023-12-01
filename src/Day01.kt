eed74f69736591f20926ccc39029d8ebab88563b
Daniel THIRION <defvs.daniel@gmail.com>
2023-12-01 11:10:49 +0100

    AoC Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun findFirstAndLastDigits(string: String) = String(charArrayOf(
+		string.first { it.isDigit() },
+		string.last { it.isDigit() }
+	)).toInt()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>) = input.sumOf { line ->
+		findFirstAndLastDigits(line)
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun String.replaceFirstAndLast(matchings: List<Pair<String, String>>): String {
+		val str1  = findAnyOf(matchings.map { it.first }, ignoreCase = true)?.let { (_, firstNum) ->
+			this.replace(firstNum, matchings.toMap()[firstNum]!! + firstNum, ignoreCase = true)
+		} ?: this
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+		return str1.findLastAnyOf(matchings.map { it.first }, ignoreCase = true)?.let { (lastNumIndex, lastNum) ->
+			str1.replaceRange(lastNumIndex, lastNumIndex+(lastNum.length), matchings.toMap()[lastNum]!!)
+		} ?: str1
+	}
+
+	val matchingPairs = listOf(
+		"one" to "1",
+		"two" to "2",
+		"three" to "3",
+		"four" to "4",
+		"five" to "5",
+		"six" to "6",
+		"seven" to "7",
+		"eight" to "8",
+		"nine" to "9",
+	)
+
+	fun part2(input: List<String>) = input.sumOf { line ->
+		findFirstAndLastDigits(line.replaceFirstAndLast(matchingPairs))
+	}
+
+	val testInput = readInput("Day01_test")
+	check(part1(testInput).also { it.println() } == 142)
+	check(part2(testInput).also { it.println() } == 281)
+
+
+	val input = readInput("Day01")
+	part1(input).println()
+	part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -12,8 +14,8 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+	.toString(16)
+	.padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
