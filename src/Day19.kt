abb01fdc2193f1f3b9d6463bcd8e1d1326cb8387
Daniel Thirion <defvs.daniel@gmail.com>
2023-12-20 12:57:01 +0100

    Cleanup Day 19

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,13 +1,19 @@
+import Day19.instructionLineRegex
+import Day19.instructionRegex
+
+object Day19 {
+	val instructionLineRegex = """([a-zA-Z]+)\{(.+)\}""".toRegex()
+	val instructionRegex = """([a-z])([<>])(\d+):([a-zA-Z]+)""".toRegex()
+}
+
 data class Part(
 	val characteristics: Map<Char, Int>
 ) {
 	fun sumOfAll() = characteristics.values.sum()
 
 	companion object {
-		fun fromString(str: String) = str.removeSurrounding("{", "}")
-			.split(",")
-			.associate { it.split("=").let { (char, int) -> char.single() to int.toInt() } }
-			.let { Part(it) }
+		fun fromString(str: String) = str.removeSurrounding("{", "}").split(",")
+			.associate { it.split("=").let { (char, int) -> char.single() to int.toInt() } }.let { Part(it) }
 	}
 }
 
@@ -15,16 +21,12 @@ data class InstructionLine(
 	val name: String,
 	val instructions: List<Instruction>,
 ) {
-	fun run(part: Part): String = instructions.first { it.operation.invoke(part) }.destination
-
+	operator fun invoke(part: Part): String = instructions.first { it.operation.invoke(part) }.destination
 
 	companion object {
 		fun fromString(str: String) =
-			"""([a-zA-Z]+)\{(.+)\}""".toRegex().matchEntire(str)!!.groupValues.let { (_, name, instructionsStr) ->
-				InstructionLine(
-					name,
-					instructionsStr.split(",").map { Instruction.fromString(it) }
-				)
+			instructionLineRegex.matchEntire(str)!!.groupValues.let { (_, name, instructionsStr) ->
+				InstructionLine(name, instructionsStr.split(",").map { Instruction.fromString(it) })
 			}
 	}
 }
@@ -40,29 +42,80 @@ data class Instruction(
 			else -> throw Exception("Invalid operation character '$char'.")
 		}
 
-		fun fromString(str: String) =
-			"""([a-z])([<>])(\d+):([a-zA-Z]+)""".toRegex().matchEntire(str)?.groupValues?.let {
-				Instruction(it[4]) { part ->
-					operationFromChar(it[2].single()).invoke(
-						part.characteristics[it[1].single()]!!,
-						it[3].toInt()
-					)
-				}
-			} ?: Instruction(str) { true }
+		fun fromString(str: String) = instructionRegex.matchEntire(str)?.groupValues?.let {
+			Instruction(it[4]) { part ->
+				operationFromChar(it[2].single()).invoke(
+					part.characteristics[it[1].single()]!!, it[3].toInt()
+				)
+			}
+		} ?: Instruction(str) { true }
 	}
 }
 
-data class Instruction2(val destination: String, val operation: (Int) -> Boolean)
+data class Operation(
+	val char: Char,
+	val op: Char,
+	val value: Int,
+	val dest: String,
+) {
+	fun split(charaMap: Map<Char, IntRange>): Pair<Map<Char, IntRange>?, Map<Char, IntRange>?> {
+		return when (op) {
+			'>' -> {
+				var mapTrue: Map<Char, IntRange>? = charaMap.mapValues {
+					if (it.key == char) it.value.let { range ->
+						(value + 1)..range.last
+					} else it.value
+				}
+				var mapFalse: Map<Char, IntRange>? = charaMap.mapValues {
+					if (it.key == char) it.value.let { range ->
+						range.first..value
+					} else it.value
+				}
+
+				if (mapTrue!!.any { it.value.first > it.value.last }) mapTrue = null
+				if (mapFalse!!.any { it.value.first > it.value.last }) mapFalse = null
+
+				mapTrue to mapFalse
+			}
+
+			'<' -> {
+				var mapTrue: Map<Char, IntRange>? = charaMap.mapValues {
+					if (it.key == char) it.value.let { range ->
+						range.first..<value
+					} else it.value
+				}
+				var mapFalse: Map<Char, IntRange>? = charaMap.mapValues {
+					if (it.key == char) it.value.let { range ->
+						value..range.last
+					} else it.value
+				}
+
+				if (mapTrue!!.any { it.value.first > it.value.last }) mapTrue = null
+				if (mapFalse!!.any { it.value.first > it.value.last }) mapFalse = null
+
+				mapTrue to mapFalse
+			}
+
+			'a' -> Pair(charaMap, null)
+
+			else -> throw Exception()
+		}
+	}
+
+	companion object {
+		fun fromString(str: String) =
+			instructionRegex.matchEntire(str)?.groupValues?.let { (_, char, op, value, dest) ->
+					Operation(char.single(), op.single(), value.toInt(), dest)
+				} ?: (Operation('a', 'a', 0, str))
+	}
+}
 
 fun main() {
 
 	fun part1(input: List<String>) = input.indexOf("").let { splitIndex ->
 		Pair(input.subList(0, splitIndex), input.subList(splitIndex + 1, input.size))
 	}.let { (instructionsStr, partsStr) ->
-		Pair(
-			instructionsStr.map { InstructionLine.fromString(it) },
-			partsStr.map { Part.fromString(it) }
-		)
+		Pair(instructionsStr.map { InstructionLine.fromString(it) }, partsStr.map { Part.fromString(it) })
 	}.let { (instructionLines, parts) ->
 		val partPositions = instructionLines.associate { it.name to (it to arrayListOf<Part>()) }
 			.let { it: Map<String, Pair<InstructionLine?, ArrayList<Part>>> ->
@@ -74,11 +127,10 @@ fun main() {
 		partPositions["in"]!!.second += parts
 
 		while (partPositions.filter { it.key !in listOf("A", "R") }.any { it.value.second.isNotEmpty() }) {
-			partPositions.filter { it.value.second.isNotEmpty() }
-				.forEach { (lineName, value) ->
+			partPositions.filter { it.value.second.isNotEmpty() }.forEach { (lineName, value) ->
 					if (value.first == null) return@forEach // do not run for A and R
 					value.second.toList().forEach { part ->
-						val destination = value.first!!.run(part)
+						val destination = value.first!!(part)
 						partPositions[lineName]!!.second.remove(part)
 						partPositions[destination]!!.second.add(part)
 					}
@@ -88,72 +140,14 @@ fun main() {
 		partPositions["A"]!!.second.sumOf { it.sumOfAll() }
 	}
 
-	data class Operation(
-		val char: Char,
-		val op: Char,
-		val value: Int,
-		val dest: String,
-	) {
-		fun split(charaMap: Map<Char, IntRange>): Pair<Map<Char, IntRange>?, Map<Char, IntRange>?> {
-			return when (op) {
-				'>' -> {
-					var mapTrue: Map<Char, IntRange>? = charaMap.mapValues {
-						if (it.key == char) it.value.let { range ->
-							(value + 1)..range.last
-						} else it.value
-					}
-					var mapFalse: Map<Char, IntRange>? = charaMap.mapValues {
-						if (it.key == char) it.value.let { range ->
-							range.first..value
-						} else it.value
-					}
-
-					if (mapTrue!!.any { it.value.first > it.value.last }) mapTrue = null
-					if (mapFalse!!.any { it.value.first > it.value.last }) mapFalse = null
-
-					mapTrue to mapFalse
-				}
-
-				'<' -> {
-					var mapTrue: Map<Char, IntRange>? = charaMap.mapValues {
-						if (it.key == char) it.value.let { range ->
-							range.first..<value
-						} else it.value
-					}
-					var mapFalse: Map<Char, IntRange>? = charaMap.mapValues {
-						if (it.key == char) it.value.let { range ->
-							value..range.last
-						} else it.value
-					}
-
-					if (mapTrue!!.any { it.value.first > it.value.last }) mapTrue = null
-					if (mapFalse!!.any { it.value.first > it.value.last }) mapFalse = null
-
-					mapTrue to mapFalse
-				}
-
-				'a' -> Pair(charaMap, null)
-
-				else -> throw Exception()
-			}
-		}
-	}
-
 	fun part2(input: List<String>) = input.indexOf("").let { input.subList(0, it) }.associate { opLineStr ->
-		"""([a-zA-Z]+)\{(.+)\}""".toRegex().matchEntire(opLineStr)!!.groupValues.let { (_, name, instructionsStr) ->
-			Pair(
-				name,
-				instructionsStr.split(",").map { opStr ->
-					"""([a-z])([<>])(\d+):([a-zA-Z]+)""".toRegex()
-						.matchEntire(opStr)?.groupValues?.let { (_, char, op, value, dest) ->
-							Operation(char.single(), op.single(), value.toInt(), dest)
-						} ?: (Operation('a', 'a', 0, opStr))
-				}
-			)
+		instructionLineRegex.matchEntire(opLineStr)!!.groupValues.let { (_, name, instructionsStr) ->
+			name to instructionsStr.split(",").map(Operation.Companion::fromString)
 		}
 	}.let { opLineMap: Map<String, List<Operation>> ->
 		val rangesPositions = opLineMap.keys.associateWith { arrayListOf<Map<Char, IntRange>>() }.let {
-			it + mapOf( // add A and R
+			it + mapOf(
+				// add A and R
 				"A" to arrayListOf(),
 				"R" to arrayListOf(),
 			)
@@ -180,8 +174,8 @@ fun main() {
 			}
 		}
 
-		rangesPositions["A"]!!.sumOf {
-			it.values.map { it.count() }.fold(1.toLong()) { acc, size -> acc * size.toLong() }.toLong()
+		rangesPositions["A"]!!.sumOf { combinations -> // count the total number of possibilities
+			combinations.values.map { it.count() }.fold(1.toLong()) { acc, size -> acc * size.toLong() }.toLong()
 		}
 	}
 
